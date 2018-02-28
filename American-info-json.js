e8fcd75c92d17153ba30369f8bb5453302c17859
ZZZ-Bin <z914820040@163.com>
2018-02-28 19:38:21 +0800

    update create json

---
+++ American-info-json.js
@@ -0,0 +1,72 @@
+American = [
+  {
+    name: "天赋异禀 第一季",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2493581990.jpg",
+    href: "https://movie.douban.com/subject/26908002/?tag=美剧&from=gaia",
+    valuation: "7.4",
+    isnew: false
+  },
+  {
+    name: "了不起的麦瑟尔夫人 第一季",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2501654012.jpg",
+    href: "https://movie.douban.com/subject/26813221/?tag=美剧&from=gaia",
+    valuation: "8.8",
+    isnew: false
+  },
+  {
+    name: "更美好的事 第二季",
+    img: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2497365699.jpg",
+    href: "https://movie.douban.com/subject/26878650/?tag=美剧&from=gaia",
+    valuation: "9.1",
+    isnew: true
+  },
+  {
+    name: "失联",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2500121804.jpg",
+    href: "https://movie.douban.com/subject/26900939/?tag=美剧&from=gaia",
+    valuation: "7.4",
+    isnew: true
+  },
+  {
+    name: "骑士陨落 第一季",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2500117861.jpg",
+    href: "https://movie.douban.com/subject/26705544/?tag=美剧&from=gaia",
+    valuation: "6.7",
+    isnew: true
+  },
+  {
+    name: "副本",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2509732403.jpg",
+    href: "https://movie.douban.com/subject/25814668/?tag=美剧&from=gaia",
+    valuation: "7.5",
+    isnew: false
+  },
+  {
+    name: "相对宇宙 第一季",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2506488961.jpg",
+    href: "https://movie.douban.com/subject/26474254/?tag=美剧&from=gaia",
+    valuation: "8.7",
+    isnew: false
+  },
+  {
+    name: "黑镜 第四季",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2507730050.jpg",
+    href: "https://movie.douban.com/subject/26898192/?tag=美剧&from=gaia",
+    valuation: "8.2",
+    isnew: false
+  },
+  {
+    name: "我们这一天 第二季",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2494940865.jpg",
+    href: "https://movie.douban.com/subject/26958597/?tag=美剧&from=gaia",
+    valuation: "9.5",
+    isnew: false
+  },
+  {
+    name: "国土安全 第二季",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2510061290.jpg",
+    href: "https://movie.douban.com/subject/26854273/?tag=美剧&from=gaia",
+    valuation: "9.4",
+    isnew: false
+  }
+]
---
+++ Hot-info-json.js
@@ -0,0 +1,73 @@
+Hot = [
+  {
+    name: "与神同行", 
+    img: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2500130777.jpg",
+    href: "https://movie.douban.com/subject/11584016/?tag=热门&from=gaia",
+    valuation: "7.9",
+    isnew: false
+  },
+  {
+    name: "三块广告牌",
+    img: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2510081688.jpg",
+    href: "https://movie.douban.com/subject/26611804/?tag=热门&from=gaia",
+    valuation: "8.7",
+    isnew: false
+  },
+  {
+    name: "游戏人生ZERO",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2441007626.jpg",
+    href: "https://movie.douban.com/subject/26837952/?tag=热门&from=gaia",
+    valuation: "8.6",
+    isnew: true
+  },
+  {
+    name: "斯大林之死",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2386120724.jpg",
+    href: "https://movie.douban.com/subject/26779885/?tag=热门&from=gaia",
+    valuation: "8.1",
+    isnew: true
+  },
+  {
+    name: "大坏狐狸的故事",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2513435650.jpg",
+    href: "https://movie.douban.com/subject/27042405/?tag=热门&from=gaia",
+    valuation: "8.3",
+    isnew: true
+  },
+  {
+    name: "芳华",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2507227732.jpg",
+    href: "https://movie.douban.com/subject/26862829/?tag=热门&from=gaia_video",
+    valuation: "7.8",
+    isnew: false
+  },
+  {
+    name: "伯德小姐",
+    img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2505925363.jpg",
+    href: "https://movie.douban.com/subject/26588314/?tag=热门&from=gaia",
+    valuation: "8.0",
+    isnew: false
+  },
+  {
+    name: "妖猫传",
+    img: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2507024497.jpg",
+    href: "https://movie.douban.com/subject/5350027/?tag=热门&from=gaia_video",
+    valuation: "7.0",
+    isnew: false
+  },
+  {
+    name: "奇迹男孩",
+    img: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2507709428.jpg",
+    href: "https://movie.douban.com/subject/26787574/?tag=热门&from=gaia",
+    valuation: "8.6",
+    isnew: false
+  },
+  {
+    name: "魅影缝匠",
+    img: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2506487968.jpg",
+    href: "https://movie.douban.com/subject/26809592/?tag=热门&from=gaia",
+    valuation: "8.0",
+    isnew: false
+  }  
+]
+
